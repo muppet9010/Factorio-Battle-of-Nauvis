fe3e4f47c414095fe19319a0966b84cfc682f950
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-04-24 21:45:43 +0000

    util base files

---
+++ constants.lua
@@ -0,0 +1,7 @@
+local Constants = {}
+
+Constants.ModName = "dropship_commander"
+Constants.AssetModName = "__" .. Constants.ModName .. "__"
+Constants.LogFileName = Constants.ModName .. "_logOutput.txt"
+
+return Constants
